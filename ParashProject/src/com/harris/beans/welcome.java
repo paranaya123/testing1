4ac69c785b4d9a0b01659dc0863bbd01487892fa
Tl <Tl@gmail.com>
2021-07-31 08:37:48 +0530

    new prg file inserted by dl

---
+++ ParashProject/src/com/harris/beans/welcome.java
@@ -0,0 +1,12 @@
+package com.harris.beans;
+
+public class welcome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("welcome to the world of java");
+		System.out.println("my name is parash nayak");
+
+	}
+
+}
