5fc9e4b6ec1d92ff168bff49c33e26c7ca1e5d34
Tl <Tl@gmail.com>
2021-07-31 08:51:54 +0530

    new programe added by tl.

---
+++ ParashProject/src/com/harris/beans/welcom.java
@@ -0,0 +1,12 @@
+package com.harris.beans;
+
+public class welcom {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("my name is parash nayak");
+
+	}
+
+}
