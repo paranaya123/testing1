f2f272c74c7ddd71c9a6e419b795e1dd8435d63d
Tl <Tl@gmail.com>
2021-07-31 09:12:41 +0530

    small changes made by tl.

--- ParashProject/src/com/harris/beans/welcom.java
+++ ParashProject/src/com/harris/beans/welcom.java
@@ -8,8 +8,8 @@ public class welcom {
 <<<<<<< HEAD
 		System.out.println("my name is parash nayak123666");
 =======
-		System.out.println("my name is parash nayak123555");
->>>>>>> branch 'master' of https://github.com/paranaya123/testing1.git
+		//System.out.println("my name is parash nayak123555");
+>>>>>>> //branch 'master' of https://github.com/paranaya123/testing1.git
 
 	}
 
